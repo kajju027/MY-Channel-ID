9e3357516779ec7d666990aa44465713c4c9090c
ꕶᴀʏᴀɴ ᴏᴀɪ 🌐 <kajju3864@gmail.com>
2025-11-08 02:12:34 +0530

    Add files via upload

---
+++ id.js
@@ -0,0 +1,107 @@
+{
+  "iframes": [
+    {
+      "id": "SHOQ",
+      "name": "CWC FINAL 🏆🫅 ",
+      "type": "iframe",
+      "iframeSrc": "https://channel-01.vercel.app/"
+    },
+    {
+      "id": "FoxTV",
+      "name": "FOX TV 🔥",
+      "type": "iframe",
+      "iframeSrc": "https://channel-04.vercel.app/"
+    },
+    {
+      "id": "HINDI",
+      "name": "CWC FINAL 🫅 (हिंदी)",
+      "type": "iframe",
+      "iframeSrc": "https://tv4live.github.io/ID11/?id=toffee1"
+    },
+    {
+      "id": "SONY1",
+      "name": "Sony Sports 1",
+      "type": "iframe",
+      "iframeSrc": "https://allinonereborn.online/sony/ptest.php?id=sony-ten-1"
+    },
+    {
+      "id": "SONY2",
+      "name": "Sony Sports 2",
+      "type": "iframe",
+      "iframeSrc": "https://allinonereborn.online/sony/ptest.php?id=sony-ten-2"
+    },
+    {
+      "id": "SONY3",
+      "name": "Sony Sports 3",
+      "type": "iframe",
+      "iframeSrc": "https://allinonereborn.online/sony/ptest.php?id=sony-ten-3"
+    },
+    {
+      "id": "SONY4",
+      "name": "Sony Sports 4",
+      "type": "iframe",
+      "iframeSrc": "https://allinonereborn.online/sony/ptest.php?id=sony-ten-4"
+    },
+     {
+      "id": "SONY5",
+      "name": "Sony Sports 5",
+      "type": "iframe",
+      "iframeSrc": "https://allinonereborn.online/sony/ptest.php?id=sony-ten-5"
+     },
+     {
+      "id": "fox",
+      "name": "Fox Cricket 501",
+      "type": "iframe",
+      "iframeSrc": "https://fox-501-allrounder745.onrender.com/"
+     },
+    {
+      "id": "TEN",
+      "name": "Ten Cricket 4K",
+      "type": "iframe",
+      "iframeSrc": "https://m.xigzo.store/?play=86db7302947d"
+    },
+    {
+      "id": "eng",
+      "name": "Star Sports 1",
+      "type": "iframe",
+      "iframeSrc": "https://m.xigzo.store/?play=d581dd66e556"
+    },
+    {
+      "id": "PKL",
+      "name": "Pro Kabaddi FINAL 🏆",
+      "type": "iframe",
+      "iframeSrc": "https://tvgo.neocities.org/APPTV/jspo?id=Star2%20HINDI%20SD"
+    },
+     {
+      "id": "astro",
+      "name": "Astro",
+      "type": "iframe",
+      "iframeSrc": "https://portal-to-m3u-07.iptv07india.workers.dev/play.m3u8?id=844"
+    },
+    {
+      "id": "Foxx",
+      "name": "Fox 🦊 TV",
+      "type": "iframe",
+      "iframeSrc": "https://channel-07.vercel.app/"
+    },
+  {
+      "id": "cwc",
+      "name": "cwc",
+      "type": "iframe",
+      "iframeSrc": "https://cricketstan.github.io/cwc/"
+    },
+    {
+      "id": "tnt-1",
+      "name": "TNT Sports 1",
+      "type": "iframe",
+      "iframeSrc": "https://cricketstan.github.io/TNT-1/"
+    },
+    {
+      "id": "prime-nz",
+      "name": "PRIME",
+      "type": "iframe",
+      "iframeSrc": "https://hff-eight.vercel.app/"
+    }
+  
+  ]
+}
